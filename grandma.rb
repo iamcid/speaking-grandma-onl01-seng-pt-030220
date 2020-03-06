452cce3b84b751dc894360699c964d1c87a7f274
Christopher Cid <iamcid17@gmail.com>
2020-03-06 04:49:31 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,11 +17,12 @@
 def speak_to_grandma(phrase)
   answer = "HUH?! SPEAK UP, SONNY!"
   if phrase == "I LOVE YOU GRANDMA!"
-    "I LOVE YOU TOO PUMPKIN!"
+    return "I LOVE YOU TOO PUMPKIN!"
   elsif phrase == phrase.upcase
-     "NO, NOT SINCE 1938!"
+    return"NO, NOT SINCE 1938!"
   else
-     answer
+     return answer
+  end
 end
 
 
